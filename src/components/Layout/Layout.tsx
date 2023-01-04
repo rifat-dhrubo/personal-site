d4a4d06aff26a09178651a6adb21c0c86d59a065
Rifat Hossain <rifat.hossain.dhrubo@gmail.com>
2023-01-04 14:38:33 +0600

    feat: base design

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,6 +6,7 @@ import { HiSun, HiMoon, HiHome } from "react-icons/hi2";
 import { useTheme } from "src/utils/darkMode";
 
 import { ClientOnly } from "../ClientOnly";
+import { Logo } from "../Logo";
 
 type Props = {
   children: React.ReactNode;
@@ -14,8 +15,9 @@ type Props = {
 export const Layout = ({ children }: Props) => {
   const { colorMode, setColorMode } = useTheme();
 
-  const [parent, enableAnimations] =
-    useAutoAnimate<HTMLButtonElement>(/* optional config */);
+  const [parent, enableAnimations] = useAutoAnimate<HTMLButtonElement>({
+    duration: 300,
+  });
   const toggleColorMode = () => {
     if (colorMode === "dark") {
       setColorMode("light");
@@ -25,14 +27,15 @@ export const Layout = ({ children }: Props) => {
   };
   return (
     <div className="min-h-screen">
-      <aside className="fixed top-0 left-1/2 mt-8 flex h-nav w-full  max-w-xl -translate-x-1/2 items-center justify-between rounded-xl border border-zinc-400/30 bg-zinc-50 p-4  backdrop-blur-sm dark:bg-zinc-800/60 md:left-0 md:mx-0 md:mt-0 md:h-full md:w-nav md:max-w-none md:translate-x-0 md:flex-col md:rounded-none md:border-0 md:border-r">
+      <aside className="fixed top-0 left-1/2 mt-8 flex h-nav w-11/12 max-w-xl -translate-x-1/2  items-center justify-between rounded-xl border border-zinc-400/30 bg-zinc-50 p-4 backdrop-blur-sm dark:bg-zinc-800/60  md:left-0 md:mx-0 md:mt-0 md:h-full  md:w-nav md:max-w-none md:translate-x-0 md:flex-col md:rounded-none md:border-0 md:border-r md:py-8">
         <div
           id="top-nav"
-          className="font-bold text-zinc-500 dark:text-zinc-300"
+          className="flex items-center gap-4 font-bold text-zinc-500 dark:text-zinc-300 md:flex-col"
         >
           <Link href="/" className="group">
             <span className="sr-only">Home</span>
-            <HiHome className="h-8 w-8 transition group-hover:scale-110" />
+            {/* <HiHome className="h-8 w-8 transition group-hover:scale-110" /> */}
+            <Logo />
           </Link>
         </div>
         <div id="bottom-nav">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,34 @@
 import React from "react";
+import Balancer from "react-wrap-balancer";
 
+import { Button } from "src/components/Button";
 import { Layout } from "src/components/Layout";
 
 const index = () => {
   return (
     <Layout>
-      <main className="min-h-screen">content</main>
+      <main className="px-8 pt-12 sm:px-16 sm:pt-20">
+        <div className="container ml-auto sm:mt-40">
+          <h1 className="text-4xl leading-tight tracking-tight sm:text-5xl sm:leading-tight">
+            <Balancer>
+              Hi, <span className="wave">👋</span> I&apos;m Rifat Hossain. An
+              avid reader and a film buff. Also, I build things for the web.
+            </Balancer>
+          </h1>
+          <p className="mt-8 max-w-4xl text-xl leading-8 dark:text-zinc-400 sm:mt-12">
+            <Balancer>
+              I&apos;m a web developer with a passion for learning and creating.
+              I&apos;ve been in this field for over 2 years now and have a
+              strong focus on building high-quality, user-friendly software
+              solutions. I enjoy working with others and am always eager to stay
+              updated on the latest technologies and trends in software
+              engineering
+            </Balancer>
+          </p>
+
+          <Button wrapperClassName="mt-8 sm:mt-24">Resume.pdf</Button>
+        </div>
+      </main>
     </Layout>
   );
 };
