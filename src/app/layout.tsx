6b829b86f7817be0de3e3c2c70895cef30dffc4e
Rifat Hossain <rifat.hossain.dhrubo@gmail.com>
2023-06-01 23:44:07 +0600

    chore: fixed eslint issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import "@/styles/globals.css"
+import "@/styles/globals.css";
 
 import type { Metadata } from "next";
 
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { ThemeProvider } from "@/components/theme-provider";
 import { siteConfig } from "@/config/site-config";
-import { cn } from "@/lib/utils";
 import { fontSans } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: {
